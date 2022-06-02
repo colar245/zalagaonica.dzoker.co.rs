eb1d0c4762efa82b0c80f5e1b3bbc28344a90596
Mladenovic Lazar <lazar.mladenovic245@gmail.com>
2022-06-03 00:29:38 +0200

    removed unused

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -16,29 +16,6 @@ $(document).ready(function($) {
 
 	"use strict";
 
-	var carousel = function() {
-		$('.owl-carousel').owlCarousel({
-			loop: true,
-			margin: 10,
-			nav: true,
-			stagePadding: 5,
-			nav: false,
-			navText: ['<span class="icon-chevron-left">', '<span class="icon-chevron-right">'],
-			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
-					items: 2
-				},
-				1000:{
-					items: 3
-				}
-			}
-		});
-	};
-	carousel();
-
 	// scroll
 	var scrollWindow = function() {
 		$(window).scroll(function(){
